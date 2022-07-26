12a2147373d30249347f6fe87716f3b18ab1f8e7
Adam La Rosa <adam.larosa@flatironschool.com>
2022-07-26 10:00:49 -0700

    062722 solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
+
+const listingsURL = 'http://localhost:6001/listings'
 
 
 function App() {
 
+  useEffect( () => {
+    fetch( listingsURL )
+      .then( resp => resp.json() )
+      .then( listingsArray =>  setListings( listingsArray ) )
+  }, [] )
+
+
+  const [ listings, setListings ] = useState( [] )
+
+  const removeListing = id => {
+    fetch( `${listingsURL}/${id}`, { method: 'DELETE' } )
+      .then( () => {
+        const newListings = listings.filter( listingObj => listingObj.id !== id  )
+        setListings( newListings )
+      } )
+  }
+
+
+  const [ search, setSearch ] = useState( '' )
+
+  const submitSearch = someString => {
+    setSearch( someString )
+  }
+
+
+  const searchedListings = listings.filter( listingObj => {
+    return listingObj.description.toLowerCase().includes( search.toLowerCase() )
+  } )
+
  
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header submitSearch={ submitSearch } />
+      <ListingsContainer removeListing={ removeListing } 
+        listings={ searchedListings }/>
     </div>
   );
 }
